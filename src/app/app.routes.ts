ce3178d58e265363fb64d9fe02f5774431da4d77
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-09 17:34:16 +0200

    Added the 404 page in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,13 @@ export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'not-found',
+        component: NotFoundComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found',
     },
 ];
