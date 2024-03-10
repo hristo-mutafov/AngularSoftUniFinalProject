5879a1958664699f40c1c7ddd4f35d63c9d88f87
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-10 15:47:50 +0200

    Created product varorites page

--- src/app/pages/product/product.routes.ts
+++ src/app/pages/product/product.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductFavoritesComponent } from './product-favorites/product-favorites.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,10 @@ export const routes: Routes = [
             {
                 path: 'add',
                 component: ProductAddComponent,
+            },
+            {
+                path: 'favorites',
+                component: ProductFavoritesComponent,
             },
             {
                 path: ':id',
