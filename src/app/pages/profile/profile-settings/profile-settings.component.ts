cfa68e06502f108c6f86bc27fa2853409509f58f
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-10 12:02:54 +0200

    Extracted the profile sidenav into separate shared component

--- src/app/pages/profile/profile-settings/profile-settings.component.ts
+++ src/app/pages/profile/profile-settings/profile-settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfileSidenavComponent } from '../../../shared/profile-sidenav/profile-sidenav.component';
 
 @Component({
     selector: 'app-profile-settings',
     standalone: true,
-    imports: [],
+    imports: [ProfileSidenavComponent],
     templateUrl: './profile-settings.component.html',
     styleUrl: './profile-settings.component.css',
 })
