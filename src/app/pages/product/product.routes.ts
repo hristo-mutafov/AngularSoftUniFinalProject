9df64e64b48e470df51f97ae25883b27c45c3403
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-10 15:35:38 +0200

    Created add-product page.

--- src/app/pages/product/product.routes.ts
+++ src/app/pages/product/product.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 
 export const routes: Routes = [
     {
         path: '',
         children: [
+            {
+                path: 'add',
+                component: ProductAddComponent,
+            },
             {
                 path: ':id',
                 component: ProductDetailsComponent,
