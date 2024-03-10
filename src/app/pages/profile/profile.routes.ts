a127cc380f0eb13de4b79d0eebb3dc7072d8da9b
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-10 12:26:03 +0200

    Created profile orders page

--- src/app/pages/profile/profile.routes.ts
+++ src/app/pages/profile/profile.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 import { ProfileAddressComponent } from './profile-address/profile-address.component';
+import { ProfileOrdersComponent } from './profile-orders/profile-orders.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,10 @@ export const routes: Routes = [
             {
                 path: 'address',
                 component: ProfileAddressComponent,
+            },
+            {
+                path: 'orders',
+                component: ProfileOrdersComponent,
             },
         ],
     },
