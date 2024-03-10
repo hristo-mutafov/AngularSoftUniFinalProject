72c6aa8ac79a9b7522284c86dc4fad7ec4deea52
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-10 16:48:47 +0200

    Register: Getting input from the form

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, FormsModule],
     templateUrl: './register.component.html',
     styleUrl: '../auth.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+    register(form: NgForm) {
+        console.log(form.valid);
+        console.log(form.value);
+    }
+}
