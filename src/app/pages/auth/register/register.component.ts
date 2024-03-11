9b2f84188582b14a17dbdbfc40d36059b636ecf4
hristo-mutafov <hristomutafov@gmail.com>
2024-03-11 10:50:11 +0200

    Register: Implemented Register and Error Handling.

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { HttpService } from '../../../core/services/http.service';
 import { EmailDirective } from '../../../shared/directives/email.directive';
 import { EqualPasswordDirective } from '../../../shared/directives/equal-password.directive';
+import {
+    AUTHENTICATION_400,
+    AUTHENTICATION_500,
+} from '../../../shared/constants';
 
 @Component({
     selector: 'app-register',
@@ -13,5 +17,28 @@ import { EqualPasswordDirective } from '../../../shared/directives/equal-passwor
     styleUrl: '../auth.css',
 })
 export class RegisterComponent {
-    register(form: NgForm) {}
+    error = signal('');
+
+    constructor(
+        private api: HttpService,
+        private router: Router,
+    ) {}
+
+    register(form: NgForm) {
+        if (form.invalid) return;
+
+        const { email, password } = form.value;
+
+        this.api.register(email, password).subscribe({
+            next: () => this.router.navigate(['home']),
+            error: (err) => {
+                const status = err.status;
+                if (status == 400) {
+                    this.error.set(AUTHENTICATION_400);
+                } else {
+                    this.error.set(AUTHENTICATION_500);
+                }
+            },
+        });
+    }
 }
