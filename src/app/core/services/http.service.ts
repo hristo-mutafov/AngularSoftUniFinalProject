58ad96d7c94a3d8f3cf7bbe949d0fcd16cee330a
hristo-mutafov <hristomutafov@gmail.com>
2024-03-11 11:27:04 +0200

    Login: Implemented Login and error handling.

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -10,4 +10,8 @@ export class HttpService {
     register(email: string, password: string) {
         return this.http.post('/api/register', { email, password });
     }
+
+    login(email: string, password: string) {
+        return this.http.post('/api/login', { email, password });
+    }
 }
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { EmailDirective } from '../../../shared/directives/email.directive';
+import { HttpService } from '../../../core/services/http.service';
+import {
+    AUTHENTICATION_400,
+    AUTHENTICATION_500,
+} from '../../../shared/constants';
 
 @Component({
     selector: 'app-login',
@@ -11,5 +16,27 @@ import { EmailDirective } from '../../../shared/directives/email.directive';
     styleUrl: '../auth.css',
 })
 export class LoginComponent {
-    login(form: NgForm) {}
+    error = signal('');
+
+    constructor(
+        private api: HttpService,
+        private router: Router,
+    ) {}
+
+    login(form: NgForm) {
+        if (form.invalid) return;
+
+        const { email, password } = form.value;
+        this.api.login(email, password).subscribe({
+            next: () => this.router.navigate(['home']),
+            error: (err) => {
+                const status = err.status;
+                if (status == 400 || status == 404) {
+                    this.error.set(AUTHENTICATION_400);
+                } else {
+                    this.error.set(AUTHENTICATION_500);
+                }
+            },
+        });
+    }
 }
