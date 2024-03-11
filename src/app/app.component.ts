97c5d0f8d9209368b6b1b9caed88da4f2d46e8b9
hristo-mutafov <hristomutafov@gmail.com>
2024-03-11 09:13:16 +0200

    Moved layout folder into core folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
     selector: 'app-root',
