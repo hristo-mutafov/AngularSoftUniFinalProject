bffa95d8c495fc3bea8bbac9702ec9b3b91f20d2
hristo-mutafov <hristomutafov@gmail.com>
2024-03-11 11:19:49 +0200

    Login: Implemented template driven form and validation.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { EmailDirective } from '../../../shared/directives/email.directive';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, FormsModule, EmailDirective],
     templateUrl: './login.component.html',
     styleUrl: '../auth.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    login(form: NgForm) {}
+}
