fe625bda2b054d485165d936d46eb3d84875d4c9
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-16 19:22:15 +0200

    Removed inital-auth route, and moved the logic inside the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './core/layout/header/header.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { TokenService } from './core/services/token.service';
+import { AuthStateService } from './core/state/auth-state.service';
+import { HttpService } from './core/services/http.service';
 
 @Component({
     selector: 'app-root',
@@ -10,6 +14,32 @@ import { FooterComponent } from './core/layout/footer/footer.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'AngularSoftUniFinalProject';
+    loading = true;
+
+    constructor(
+        private tokenService: TokenService,
+        private authState: AuthStateService,
+        private http: HttpService,
+    ) {}
+
+    ngOnInit(): void {
+        const accessToken = this.tokenService.accessToken;
+        if (accessToken) {
+            this.authState.authenticate(accessToken);
+            this.loading = false;
+        } else {
+            const refreshToken = this.tokenService.refreshToken;
+
+            if (refreshToken) {
+                this.http
+                    .getAccessToken(refreshToken)
+                    .subscribe(() => (this.loading = false));
+            } else {
+                this.authState.unAuthenticate();
+                this.loading = false;
+            }
+        }
+    }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,48 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { initialAuthGuard } from './core/guards/initial-auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
-        canActivate: [initialAuthGuard],
-        children: [
-            {
-                path: '',
-                pathMatch: 'full',
-                redirectTo: '/home',
-            },
-            {
-                path: 'home',
-                component: HomeComponent,
-            },
-            {
-                path: 'auth',
-                loadChildren: () =>
-                    import('./pages/auth/auth.routes').then((r) => r.routes),
-            },
-            {
-                path: 'product',
-                loadChildren: () =>
-                    import('./pages/product/product.routes').then(
-                        (r) => r.routes,
-                    ),
-            },
-            {
-                path: 'profile',
-                loadChildren: () =>
-                    import('./pages/profile/profile.routes').then(
-                        (r) => r.routes,
-                    ),
-            },
-            {
-                path: 'cart',
-                loadChildren: () =>
-                    import('./pages/cart/cart.routes').then((r) => r.routes),
-            },
-            {
-                path: 'order',
-                loadChildren: () =>
-                    import('./pages/order/order.routes').then((r) => r.routes),
-            },
-            {
-                path: 'not-found',
-                component: NotFoundComponent,
-            },
-            {
-                path: '**',
-                redirectTo: 'not-found',
-            },
-        ],
+        pathMatch: 'full',
+        redirectTo: '/home',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'auth',
+        loadChildren: () =>
+            import('./pages/auth/auth.routes').then((r) => r.routes),
+    },
+    {
+        path: 'product',
+        loadChildren: () =>
+            import('./pages/product/product.routes').then((r) => r.routes),
+    },
+    {
+        path: 'profile',
+        loadChildren: () =>
+            import('./pages/profile/profile.routes').then((r) => r.routes),
+    },
+    {
+        path: 'cart',
+        loadChildren: () =>
+            import('./pages/cart/cart.routes').then((r) => r.routes),
+    },
+    {
+        path: 'order',
+        loadChildren: () =>
+            import('./pages/order/order.routes').then((r) => r.routes),
+    },
+    {
+        path: 'not-found',
+        component: NotFoundComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found',
     },
 ];
