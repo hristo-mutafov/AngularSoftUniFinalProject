a52a74eac918824f2ad4d92f53fbf7cb6efa7bcc
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-19 17:39:29 +0200

    Fixed the condition inside the cart.component.html and removed unused imports inside the cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../../core/services/http.service';
-import { IGetCartResponse } from '../../types';
-import { catchError, retry, throwError } from 'rxjs';
-import { ERROR_RETRY_TIMES } from '../../shared/constants';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { GetWholePricePipe } from '../../shared/pipes/get-whole-price.pipe';
-import { GetDecimalPartPipe } from '../../shared/pipes/get-decimal-part.pipe';
+import { catchError, retry, throwError } from 'rxjs';
+
+import { HttpService } from '../../core/services/http.service';
+import { IGetCartResponse } from '../../types';
+import { ERROR_RETRY_TIMES } from '../../shared/constants';
 import { getFutureDate } from '../../shared/utils';
-import { LoaderComponent } from '../../shared/components/loader/loader.component';
 import { EmptyCartComponent } from '../../shared/components/cart/empty-cart/empty-cart.component';
 import { CartTableComponent } from '../../shared/components/cart/cart-table/cart-table.component';
 import { CartPriceMenuComponent } from '../../shared/components/cart/cart-price-menu/cart-price-menu.component';
@@ -16,18 +14,12 @@ import { CartPriceMenuComponent } from '../../shared/components/cart/cart-price-
 @Component({
     selector: 'app-cart',
     standalone: true,
-    imports: [
-        GetWholePricePipe,
-        GetDecimalPartPipe,
-        LoaderComponent,
-        EmptyCartComponent,
-        CartTableComponent,
-        CartPriceMenuComponent,
-    ],
+    imports: [EmptyCartComponent, CartTableComponent, CartPriceMenuComponent],
     templateUrl: './cart.component.html',
     styleUrl: './cart.component.css',
 })
 export class CartComponent implements OnInit {
+    // TODO: Check if empty cart works
     isLoading = true;
     cart: IGetCartResponse | null = null;
     dateAfterFourDays = getFutureDate(4);
