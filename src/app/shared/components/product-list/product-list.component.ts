324ab51ca2e6e82ecca52dd619b9e54ee87418e6
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-19 10:13:09 +0200

    Created dynamic routing for products

--- src/app/shared/components/product-list/product-list.component.ts
+++ src/app/shared/components/product-list/product-list.component.ts
@@ -1,20 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { IProductList } from '../../../types';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-product-list',
     standalone: true,
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './product-list.component.html',
     styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
     // TODO: Animated No-products
     @Input() products: IProductList | null = null;
-
-    product = {
-        image: 'https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg',
-        name: 'Demo Demo',
-        price: 33.33,
-    };
 }
