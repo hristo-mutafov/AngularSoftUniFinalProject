99397b2c36e1a5f9280821ee575a4daef12458aa
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-20 22:37:56 +0200

    Implemented template-driven forms inside the order-delivery-component

--- src/app/shared/components/order/order-delivery-method/order-delivery-method.component.ts
+++ src/app/shared/components/order/order-delivery-method/order-delivery-method.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-order-delivery-method',
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './order-delivery-method.component.html',
     styleUrl: '../method-and-payment-common.css',
 })
-export class OrderDeliveryMethodComponent {}
+export class OrderDeliveryMethodComponent {
+    submitDeliveryMethodOption(form: NgForm) {
+        console.log(form.value);
+    }
+
+    submitDeliveryUserInformation(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+        console.log(form.value);
+    }
+}
