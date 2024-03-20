480a5dc406e2490095faf2c6bd20c09bb0192ce6
Hristo Mutafov <hristomutafov@gmail.com>
2024-03-20 22:50:06 +0200

    Implemented template-driven form in order comment component

--- src/app/shared/components/order/order-comment/order-comment.component.ts
+++ src/app/shared/components/order/order-comment/order-comment.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-order-comment',
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './order-comment.component.html',
     styleUrl: './order-comment.component.css',
 })
-export class OrderCommentComponent {}
+export class OrderCommentComponent {
+    submitComment(form: NgForm) {
+        console.log(form.value);
+    }
+}
